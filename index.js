0413d418a32e24a763b411b12dbed6ab666def0c
JAIGOPAL83 <jaigopal.senapati@gmail.com>
2024-04-26 12:54:11 +1000

    Change to V1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! V4",
+        message: "Go Serverless v3.0! V1",
         input: event,
       },
       null,
